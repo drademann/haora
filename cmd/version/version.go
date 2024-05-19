2b0a1ec6c04ab0931e3c77732bf55bd20307d05a
Dirk Rademann <dirk.rademann@icloud.com>
2024-05-19 17:49:18 +0200

    sets version for release

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -21,7 +21,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 func init() {
 	root.Command.AddCommand(command)
--- cmd/version/version_test.go
+++ cmd/version/version_test.go
@@ -25,7 +25,7 @@ import (
 func TestVersionCmd(t *testing.T) {
 	out := test.ExecuteCommand(t, root.Command, "version")
 
-	expected := "Haora v1.0.0\n"
+	expected := "Haora v1.0.1\n"
 	if out.String() != expected {
 		t.Errorf("expected output %q but got %q", expected, out.String())
 	}

390b6d380461a811a7d66994ba231a0de64120bb
Dirk Rademann <dirk.rademann@icloud.com>
2024-06-23 16:53:12 +0200

    sets version for release

--- cmd/version/version_test.go
+++ cmd/version/version_test.go
@@ -25,7 +25,7 @@ import (
 func TestVersionCmd(t *testing.T) {
 	out := test.ExecuteCommand(t, root.Command, "version")
 
-	expected := "Haora v1.0.1\n"
+	expected := "Haora v1.1.0\n"
 	if out.String() != expected {
 		t.Errorf("expected output %q but got %q", expected, out.String())
 	}

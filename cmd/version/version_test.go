e930bd6cb6193b223b67996de0ff568f6d232073
Dirk Rademann <dirk.rademann@icloud.com>
2025-01-25 17:25:20 +0100

    chore: sets a new version for release

--- cmd/version/version_test.go
+++ cmd/version/version_test.go
@@ -25,7 +25,7 @@ import (
 func TestVersionCmd(t *testing.T) {
 	out := cmd.TestExecute(t, root.Command, "version")
 
-	expected := "Haora v1.6.0-beta.4\n"
+	expected := "Haora v1.6.0\n"
 	if out.String() != expected {
 		t.Errorf("expected output %q but got %q", expected, out.String())
 	}
